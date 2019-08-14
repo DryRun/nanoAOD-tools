3478b9b0559cd0fb57d465f435797e2da525b2f2
DryRun <david.renhwa.yu@gmail.com>
2019-08-14 14:35:32 -0500

    Fix mixed tabs and spaces with autopep8 (E101 and E121)

--- python/postprocessing/framework/postprocessor.py
+++ python/postprocessing/framework/postprocessor.py
@@ -14,27 +14,27 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.jobreport import JobRepo
 
 class PostProcessor :
     def __init__(self,outputDir,inputFiles,cut=None,branchsel=None,modules=[],compression="LZMA:9",friend=False,postfix=None,
-		 jsonInput=None,noOut=False,justcount=False,provenance=False,haddFileName=None,fwkJobReport=False,histFileName=None,histDirName=None, outputbranchsel=None,maxEntries=None,firstEntry=0,
-		 prefetch=False,longTermCache=False):
-	self.outputDir=outputDir
-	self.inputFiles=inputFiles
-	self.cut=cut
-	self.modules=modules
-	self.compression=compression
-	self.postfix=postfix
-	self.json=jsonInput
-	self.noOut=noOut
-	self.friend=friend
-	self.justcount=justcount
-	self.provenance=provenance
-	self.jobReport = JobReport() if fwkJobReport else None
-	self.haddFileName=haddFileName
-	self.histFile = None
-	self.histDirName = None
-	if self.jobReport and not self.haddFileName :
-		print "Because you requested a FJR we assume you want the final hadd. No name specified for the output file, will use tree.root"
-		self.haddFileName="tree.root"
- 	self.branchsel = BranchSelection(branchsel) if branchsel else None 
+                 jsonInput=None,noOut=False,justcount=False,provenance=False,haddFileName=None,fwkJobReport=False,histFileName=None,histDirName=None, outputbranchsel=None,maxEntries=None,firstEntry=0,
+                 prefetch=False,longTermCache=False):
+        self.outputDir=outputDir
+        self.inputFiles=inputFiles
+        self.cut=cut
+        self.modules=modules
+        self.compression=compression
+        self.postfix=postfix
+        self.json=jsonInput
+        self.noOut=noOut
+        self.friend=friend
+        self.justcount=justcount
+        self.provenance=provenance
+        self.jobReport = JobReport() if fwkJobReport else None
+        self.haddFileName=haddFileName
+        self.histFile = None
+        self.histDirName = None
+        if self.jobReport and not self.haddFileName :
+            print "Because you requested a FJR we assume you want the final hadd. No name specified for the output file, will use tree.root"
+            self.haddFileName="tree.root"
+        self.branchsel = BranchSelection(branchsel) if branchsel else None 
         self.outputbranchsel = BranchSelection(outputbranchsel) if outputbranchsel else None
         self.histFileName=histFileName
         self.histDirName=histDirName
@@ -68,8 +68,8 @@ class PostProcessor :
             return fname, False
     def run(self) :
         outpostfix = self.postfix if self.postfix != None else ("_Friend" if self.friend else "_Skim")
-    	if not self.noOut:
-            
+        if not self.noOut:
+
             if self.compression != "none":
                 ROOT.gInterpreter.ProcessLine("#include <Compression.h>")
                 (algo, level) = self.compression.split(":")
@@ -80,16 +80,16 @@ class PostProcessor :
                 else: raise RuntimeError("Unsupported compression %s" % algo)
             else:
                 compressionLevel = 0 
-	    print "Will write selected trees to "+self.outputDir
+            print "Will write selected trees to "+self.outputDir
             if not self.justcount:
                 if not os.path.exists(self.outputDir):
                     os.system("mkdir -p "+self.outputDir)
         else:
             compressionLevel = 0
 
-	if self.noOut:
-	    if len(self.modules) == 0: 
-		raise RuntimeError("Running with --noout and no modules does nothing!")
+        if self.noOut:
+            if len(self.modules) == 0: 
+                raise RuntimeError("Running with --noout and no modules does nothing!")
 
         # Open histogram file, if desired 
         if (self.histFileName != None and self.histDirName == None) or (self.histFileName == None and self.histDirName != None) :
@@ -99,61 +99,61 @@ class PostProcessor :
         else :
             self.histFile = None
 
-    
+
         for m in self.modules:
             if hasattr( m, 'writeHistFile') and m.writeHistFile :
                 m.beginJob(histFile=self.histFile,histDirName=self.histDirName)
             else :
                 m.beginJob()
 
-	fullClone = (len(self.modules) == 0)
-	outFileNames=[]
+        fullClone = (len(self.modules) == 0)
+        outFileNames=[]
         t0 = time.clock()
-	totEntriesRead=0
-	for fname in self.inputFiles:
-	    ffnames = []
-	    if "," in fname:
-	        fnames = fname.split(',')
-	        fname, ffnames = fnames[0], fnames[1:]
+        totEntriesRead=0
+        for fname in self.inputFiles:
+            ffnames = []
+            if "," in fname:
+                fnames = fname.split(',')
+                fname, ffnames = fnames[0], fnames[1:]
 
-	    # open input file
-	    if self.prefetch:
-	        ftoread, toBeDeleted = self.prefetchFile(fname)
-	        inFile = ROOT.TFile.Open(ftoread)
-	    else:
-	        inFile = ROOT.TFile.Open(fname)
+            # open input file
+            if self.prefetch:
+                ftoread, toBeDeleted = self.prefetchFile(fname)
+                inFile = ROOT.TFile.Open(ftoread)
+            else:
+                inFile = ROOT.TFile.Open(fname)
 
-	    #get input tree
-	    inTree = inFile.Get("Events")
-	    if inTree == None: inTree = inFile.Get("Friends")
-	    nEntries = min(inTree.GetEntries() - self.firstEntry, self.maxEntries)
-	    totEntriesRead+=nEntries
-	    # pre-skimming
-	    elist,jsonFilter = preSkim(inTree, self.json, self.cut, maxEntries = self.maxEntries, firstEntry = self.firstEntry)
-	    if self.justcount:
-		print 'Would select %d / %d entries from %s (%.2f%%)'%(elist.GetN() if elist else nEntries, nEntries, fname, (elist.GetN() if elist else nEntries)/(0.01*nEntries) if nEntries else 0)
-		if self.prefetch:
-		    if toBeDeleted: os.unlink(ftoread)
-		continue
-	    else:
-		print 'Pre-select %d entries out of %s (%.2f%%)'%(elist.GetN() if elist else nEntries,nEntries,(elist.GetN() if elist else nEntries)/(0.01*nEntries) if nEntries else 0)
-		inAddFiles = []
-		inAddTrees = []
-	    for ffname in ffnames:
-		inAddFiles.append(ROOT.TFile.Open(ffname))
-		inAddTree = inAddFiles[-1].Get("Events")
-		if inAddTree == None: inAddTree = inAddFiles[-1].Get("Friends")
-		inAddTrees.append(inAddTree)
-		inTree.AddFriend(inAddTree)
+            #get input tree
+            inTree = inFile.Get("Events")
+            if inTree == None: inTree = inFile.Get("Friends")
+            nEntries = min(inTree.GetEntries() - self.firstEntry, self.maxEntries)
+            totEntriesRead+=nEntries
+            # pre-skimming
+            elist,jsonFilter = preSkim(inTree, self.json, self.cut, maxEntries = self.maxEntries, firstEntry = self.firstEntry)
+            if self.justcount:
+                print 'Would select %d / %d entries from %s (%.2f%%)'%(elist.GetN() if elist else nEntries, nEntries, fname, (elist.GetN() if elist else nEntries)/(0.01*nEntries) if nEntries else 0)
+                if self.prefetch:
+                    if toBeDeleted: os.unlink(ftoread)
+                continue
+            else:
+                print 'Pre-select %d entries out of %s (%.2f%%)'%(elist.GetN() if elist else nEntries,nEntries,(elist.GetN() if elist else nEntries)/(0.01*nEntries) if nEntries else 0)
+                inAddFiles = []
+                inAddTrees = []
+            for ffname in ffnames:
+                inAddFiles.append(ROOT.TFile.Open(ffname))
+                inAddTree = inAddFiles[-1].Get("Events")
+                if inAddTree == None: inAddTree = inAddFiles[-1].Get("Friends")
+                inAddTrees.append(inAddTree)
+                inTree.AddFriend(inAddTree)
 
-	    if fullClone:
-		# no need of a reader (no event loop), but set up the elist if available
-		if elist: inTree.SetEntryList(elist)
-	    else:
-		# initialize reader
-		inTree = InputTree(inTree, elist) 
+            if fullClone:
+                # no need of a reader (no event loop), but set up the elist if available
+                if elist: inTree.SetEntryList(elist)
+            else:
+                # initialize reader
+                inTree = InputTree(inTree, elist) 
 
-	    # prepare output file
+            # prepare output file
             if not self.noOut:
                 outFileName = os.path.join(self.outputDir, os.path.basename(fname).replace(".root",outpostfix+".root"))
                 outFile = ROOT.TFile.Open(outFileName, "RECREATE", "", compressionLevel)
@@ -179,35 +179,35 @@ class PostProcessor :
                 outFile = None
                 outTree = None
                 if self.branchsel:
-                  self.branchsel.selectBranches(inTree)
+                    self.branchsel.selectBranches(inTree)
 
-	    # process events, if needed
-	    if not fullClone:
+            # process events, if needed
+            if not fullClone:
                 eventRange = xrange(self.firstEntry, self.firstEntry + nEntries) if nEntries > 0 and not elist else None
-		(nall, npass, timeLoop) = eventLoop(self.modules, inFile, outFile, inTree, outTree, eventRange=eventRange, maxEvents=self.maxEntries)
-		print 'Processed %d preselected entries from %s (%s entries). Finally selected %d entries' % (nall, fname, nEntries, npass)
-	    else:
+                (nall, npass, timeLoop) = eventLoop(self.modules, inFile, outFile, inTree, outTree, eventRange=eventRange, maxEvents=self.maxEntries)
+                print 'Processed %d preselected entries from %s (%s entries). Finally selected %d entries' % (nall, fname, nEntries, npass)
+            else:
                 nall = nEntries
-		print 'Selected %d / %d entries from %s (%.2f%%)' % (outTree.tree().GetEntries(), nall, fname, outTree.tree().GetEntries()/(0.01*nall) if nall else 0)
+                print 'Selected %d / %d entries from %s (%.2f%%)' % (outTree.tree().GetEntries(), nall, fname, outTree.tree().GetEntries()/(0.01*nall) if nall else 0)
 
-	    # now write the output
+            # now write the output
             if not self.noOut: 
                 outTree.write()
                 outFile.Close()
                 print "Done %s" % outFileName
-	    if self.jobReport:
-		self.jobReport.addInputFile(fname,nall)
-	    if self.prefetch:
-		if toBeDeleted: os.unlink(ftoread)
-		
-	for m in self.modules: m.endJob()
-	
-	print  totEntriesRead/(time.clock()-t0), "Hz"
+            if self.jobReport:
+                self.jobReport.addInputFile(fname,nall)
+            if self.prefetch:
+                if toBeDeleted: os.unlink(ftoread)
+
+        for m in self.modules: m.endJob()
+
+        print  totEntriesRead/(time.clock()-t0), "Hz"
 
 
-	if self.haddFileName :
-		haddnano = "./haddnano.py" if os.path.isfile("./haddnano.py") else "haddnano.py"
-		os.system("%s %s %s" %(haddnano, self.haddFileName," ".join(outFileNames)))
-	if self.jobReport :
-		self.jobReport.addOutputFile(self.haddFileName)
-		self.jobReport.save()
+        if self.haddFileName :
+            haddnano = "./haddnano.py" if os.path.isfile("./haddnano.py") else "haddnano.py"
+            os.system("%s %s %s" %(haddnano, self.haddFileName," ".join(outFileNames)))
+        if self.jobReport :
+            self.jobReport.addOutputFile(self.haddFileName)
+            self.jobReport.save()
